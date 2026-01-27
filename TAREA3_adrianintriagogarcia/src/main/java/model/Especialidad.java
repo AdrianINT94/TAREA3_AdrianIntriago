017e9403734dd701e8528d4a4995ae40f8ef74c2
AdrianINT94 <wkj62135@educastur.es>
2026-01-27 14:39:31 +0100

    Delete TAREA3_adrianintriagogarcia directory

--- TAREA3_adrianintriagogarcia/src/main/java/model/Especialidad.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum Especialidad {
-	ACROBACIA,HUMOR,MAGIA,EQUILIBRISMO,MALABARISMO
-
-}
--- TAREA3_adrianintriagogarcia/src/main/java/model/Rol.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Rol {
-	ADMIN,COORDINADOR,ARTISTA
-}
